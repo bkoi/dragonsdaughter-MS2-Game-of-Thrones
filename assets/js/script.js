e1a5397a1fce3ecf954cd488221383e7adeca86d
bkoi <bernadettkoi@gmail.com>
2021-08-22 09:05:53 +0000

    Fix timer and flip counter position

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ let cards = document.querySelectorAll('.game-card');
 
 function cardFlip(cards) {
     this.classList.toggle('flip');
-    console.log('flipping');
 }
 
 cards.forEach((card) => card.addEventListener('click', cardFlip));
